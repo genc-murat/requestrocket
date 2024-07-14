a824aef1fc8842bf6f4169519f63c5cfb0f5cc8b
Murat Genc <muttafi@gmail.com>
2024-07-14 23:02:00 +0300

    chore: Update TitleBar component with SVG icons for minimize, maximize, and close buttons

--- src/components/TitleBar.svelte
+++ src/components/TitleBar.svelte
@@ -21,9 +21,42 @@
         <div class="title">Request Rocket</div>
     </div>
     <div class="buttons">
-        <button on:click={minimize}>_</button>
-        <button on:click={maximize}>[]</button>
-        <button on:click={close}>×</button>
+        <button on:click={minimize}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+            >
+                <path fill="currentColor" d="M19 13H5v-2h14v2z" />
+            </svg>
+        </button>
+        <button on:click={maximize}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+            >
+                <path
+                    fill="currentColor"
+                    d="M3 5v14h18V5H3zm16 12H5V7h14v10z"
+                />
+            </svg>
+        </button>
+        <button on:click={close}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+            >
+                <path
+                    fill="currentColor"
+                    d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+                />
+            </svg>
+        </button>
     </div>
 </div>
 
@@ -75,5 +108,11 @@
 
     .titlebar button:hover {
         color: var(--error);
+    }
+
+    .titlebar button svg {
+        width: 20px;
+        height: 20px;
+        display: block;
     }
 </style>
