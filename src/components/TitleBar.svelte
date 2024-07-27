a456b162199cb170e6de6150c2264558e365230a
Murat Genc <muttafi@gmail.com>
2024-07-27 22:46:46 +0300

    Refactor TitleBar component to update button sizes and layout

--- src/components/TitleBar.svelte
+++ src/components/TitleBar.svelte
@@ -29,23 +29,23 @@
     <div class="buttons">
         <button on:click={toggleLayout} title="Toggle Layout">
             {#if $currentLayout === "default"}
-                <Icon icon="fluent:layout-column-two-focus-right-20-filled" width="24" height="24" />
+                <Icon icon="fluent:layout-column-two-focus-right-20-filled" width="20" height="20" />
             {:else}
-                <Icon icon="fluent:layout-row-two-focus-bottom-16-filled" width="24" height="24" />
+                <Icon icon="fluent:layout-row-two-focus-bottom-16-filled" width="20" height="20" />
             {/if}
         </button>
         <button on:click={minimize}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
                 <path fill="currentColor" d="M19 13H5v-2h14v2z" />
             </svg>
         </button>
         <button on:click={maximize}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
                 <path fill="currentColor" d="M3 5v14h18V5H3zm16 12H5V7h14v10z" />
             </svg>
         </button>
         <button on:click={close}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
                 <path fill="currentColor" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
             </svg>
         </button>
@@ -107,8 +107,8 @@
         transform: translateY(-1px);
     }
     .titlebar button svg {
-        width: 20px;
-        height: 20px;
+        width: 18px;
+        height: 18px;
         display: block;
         filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.1));
     }
