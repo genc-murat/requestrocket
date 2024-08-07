0487e369816d70a0137d6aa672e3ecf64afb6587
Murat Genc <muttafi@gmail.com>
2024-08-07 11:51:32 +0300

    refactor: Adjust font size and position in app.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2115,7 +2115,7 @@
                         on:click={() => selectHistoryItem(item)}
                       >
                         <span
-                          class="px-2 py-1 rounded
+                          class="px-2 py-1 rounded h-0.5
                         {item.method === 'GET' ? 'method-get' : ''} 
                         {item.method === 'POST' ? 'method-post' : ''} 
                         {item.method === 'PUT' ? 'method-put' : ''} 
